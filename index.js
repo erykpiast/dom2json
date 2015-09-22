3d5a1b7dfc8664aae1e41110813c598a808b7e48
Sebastian Krzyszkowiak <s.krzyszkowiak@cogision.com>
2015-09-22 15:54:00 +0200

    Error handling++

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ module.exports = (function() {
                 try {
                     node.setAttribute(attrName, attrValue);
                 } catch(e) {
-                    console.error('[dom2json] Couldn\'t set attribute "' + attrName + '" to "' + attrValue + '"', e, node);
+                    console.error('[dom2json] Couldn\'t set attribute "' + attrName + '" to "' + attrValue + '"', e, token, node);
                 }
             });
         }
